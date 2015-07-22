3042edccbe3dec854c21b1196320837d07824e56
Phuc <mrtrongphuc@gmail.com>
2015-07-23 00:46:11 +0700

    Calculate the height of keyboard on Android >= 5.0

--- app/src/main/java/com/trongphuc/emoappchat/BaseActivity.java
+++ app/src/main/java/com/trongphuc/emoappchat/BaseActivity.java
@@ -1,13 +1,19 @@
 package com.trongphuc.emoappchat;
 
+import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Point;
 import android.graphics.Rect;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 import android.widget.RelativeLayout;
 
 /**
@@ -42,8 +48,12 @@ public abstract class BaseActivity extends AppCompatActivity {
                         Rect r = new Rect();
                         parentLayout.getWindowVisibleDisplayFrame(r);
 
-                        int screenHeight = parentLayout.getRootView()
-                                .getHeight();
+                        int screenHeight;
+                        if (Build.VERSION.SDK_INT >= 5.0) {
+                            screenHeight = calculateScreenHeightForLollipop();
+                        } else {
+                            screenHeight = parentLayout.getRootView().getHeight();
+                        }
                         int keyboardHeight = screenHeight - (r.bottom);
                         if (HardwareUtils.hasNavBar(BaseActivity.this)) {
                             int resourceId = getResources()
@@ -87,5 +97,14 @@ public abstract class BaseActivity extends AppCompatActivity {
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
                 RelativeLayout.LayoutParams.MATCH_PARENT, savedHeight);
         stickerSet.setLayoutParams(params);
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    public int calculateScreenHeightForLollipop() {
+        WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        return size.y;
     }
 }
